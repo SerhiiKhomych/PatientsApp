4f18c9fd253e8cdec9a32fa5c6975849a0107d45
Serhii Khomych <serhii.khomych-ext@censhare.com>
2019-07-13 11:26:34 +0300

    another iteration

--- MyPatient/Controllers/DailyPatientsViewController.swift
+++ MyPatient/Controllers/DailyPatientsViewController.swift
@@ -20,6 +20,16 @@ class DailyPatientsViewController: UITableViewController {
     var googleDriveService: GTLRDriveService!
     var googleUser: GIDGoogleUser!
     
+    /// View which contains the loading text and the spinner
+    let loadingView = UIView()
+    
+    /// Spinner shown during load the TableView
+    let spinner = UIActivityIndicatorView()
+    
+    /// Text shown during load the TableView
+    let loadingLabel = UILabel()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellIdentifier")
@@ -104,11 +114,13 @@ class DailyPatientsViewController: UITableViewController {
     }
     
     @IBAction func sendToTheCloud(_ sender: Any) {
+        setLoadingScreen()
+
         let googleDriveApiService = GoogleDriveApiService()
         let group = DispatchGroup()
-        group.enter()
         if let indexPaths = tableView.indexPathsForSelectedRows {
             for indexPath in indexPaths {
+                group.enter()
                 let directoryName = items[indexPath.row]
                 let fileManager = FileManager.default
                 
@@ -148,6 +160,8 @@ class DailyPatientsViewController: UITableViewController {
                 }
             }
             group.notify(queue:.main) {
+                self.removeLoadingScreen()
+                
                 self.editCancel.title = "Choose"
                 self.cloud.isEnabled = false
                 
@@ -155,5 +169,47 @@ class DailyPatientsViewController: UITableViewController {
                 self.tableView.setEditing(false, animated: false)
             }
         }
+    }
+    
+    
+    // Set the activity indicator into the main view
+    private func setLoadingScreen() {
+        
+        // Sets the view which contains the loading text and the spinner
+        let width: CGFloat = 120
+        let height: CGFloat = 30
+        let x = (tableView.frame.width / 2) - (width / 2)
+        let y = (tableView.frame.height / 2) - (height / 2) - (navigationController?.navigationBar.frame.height)!
+        loadingView.frame = CGRect(x: x, y: y, width: width, height: height)
+        
+        // Sets loading text
+        loadingLabel.isHidden = false
+        loadingLabel.textColor = .gray
+        loadingLabel.textAlignment = .center
+        loadingLabel.text = "Loading..."
+        loadingLabel.frame = CGRect(x: 0, y: 0, width: 140, height: 30)
+        
+        // Sets spinner
+        spinner.isHidden = false
+        spinner.style = .gray
+        spinner.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        spinner.startAnimating()
+        
+        // Adds text and spinner to the view
+        loadingView.addSubview(spinner)
+        loadingView.addSubview(loadingLabel)
+        
+        tableView.addSubview(loadingView)
+        
+    }
+    
+    // Remove the activity indicator from the main view
+    private func removeLoadingScreen() {
+        
+        // Hides and stops the text and the spinner
+        spinner.stopAnimating()
+        spinner.isHidden = true
+        loadingLabel.isHidden = true
+        
     }
 }
